0272087512825e9c2194191d6a321ec9bd57b938
tuyenbv <tuyenbv@smartosc.com>
2014-07-30 15:59:19 +0700

    tuyenbv acpl newproduct

---
+++ app/code/local/Group2/Acpl/controllers/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+class Group2_Acpl_IndexController extends Mage_Core_Controller_Front_Action {
+
+    public function indexAction() {
+
+        echo 'xin chao';
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+    public function anotherAction() {
+        
+    }
+
+
+}
+
+?>
