2152de1eac54c27c6716d5fbf652c41a820ae2bc
tuyenbv <tuyenbv@smartosc.com>
2014-07-30 16:29:55 +0700

    tuyenbv module acpl addCmsPageLayout

--- app/code/local/Teamto/Acpl/controllers/IndexController.php
+++ app/code/local/Teamto/Acpl/controllers/IndexController.php
@@ -1,10 +1,9 @@
 <?php
 
-class Group2_Acpl_IndexController extends Mage_Core_Controller_Front_Action {
+class Teamto_Acpl_IndexController extends Mage_Core_Controller_Front_Action {
 
     public function indexAction() {
 
-        echo 'xin chao';
         $this->loadLayout();
         $this->renderLayout();
     }
