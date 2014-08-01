04d6c879ce798c1dffbeadaf4ecd7f3556c0bd7d
datdb <datdb@smartosc.com>
2014-08-01 14:54:03 +0700

    commit filter module

--- app/code/local/Teamto/Filter/sql/teamto_filter_setup/mysql4-install-0.1.0.php
+++ app/code/local/Teamto/Filter/sql/teamto_filter_setup/mysql4-install-0.1.0.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: root
- * Date: 7/29/14
- * Time: 2:28 PM
+ * User: kevin
+ * Date: 7/30/14
+ * Time: 10:55 AM
  */ 
 /* @var $installer Mage_Core_Model_Resource_Setup */
 $installer = $this;
