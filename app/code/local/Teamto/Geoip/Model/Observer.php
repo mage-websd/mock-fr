d9ae83aa6e03c12740cbd6b714220acd25ab52d2
tuyenbv <tuyenbv@smartosc.com>
2014-08-05 11:45:34 +0700

    media

--- app/code/local/Teamto/Geoip/Model/Observer.php
+++ app/code/local/Teamto/Geoip/Model/Observer.php
@@ -5,7 +5,7 @@ class Teamto_Geoip_Model_Observer {
     public function geoip_store() {
   //      $geoIP = Mage::getSingleton('geoip/country');
 //        $countryCode = $geoIP->getCountry();
-       Mage::app()->getResponse()->setRedirect(Mage::getUrl('checkout/cart'));
+      // Mage::app()->getResponse()->setRedirect(Mage::getUrl('checkout/cart'));
 //        die;
 //        switch ($countryCode) {
 //            
