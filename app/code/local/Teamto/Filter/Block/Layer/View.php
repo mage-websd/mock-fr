4da43cc014641910a4e07cb1a5600fa2207a0be5
datdb <datdb@smartosc.com>
2014-08-05 15:46:59 +0700

    commit to pull

--- app/code/local/Teamto/Filter/Block/Layer/View.php
+++ app/code/local/Teamto/Filter/Block/Layer/View.php
@@ -4,5 +4,14 @@ class Teamto_Filter_Block_Layer_View extends Mage_Catalog_Block_Layer_View {
 
     public function getPriceRange() {
         return Mage::getModel('catalog/layer_filter_price')->getMaxPriceInt();
+
+//        $maxPrice = Mage::getModel('catalog/layer_filter_price')->getMaxPriceInt();
+//
+//        var_dump(Mage::getSingleton('core/session'));die;
+//
+//        if(!iss){
+//            Mage::getSingleton('core/session')->setMaxPrice($maxPrice);
+//        }
+//        return ;
     }
 }
