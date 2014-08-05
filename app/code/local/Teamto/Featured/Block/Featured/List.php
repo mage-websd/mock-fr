d56b62457faf37ecd0d66632f8e333f1c0b090bb
datdb <datdb@smartosc.com>
2014-08-05 14:18:10 +0700

    fix bug filter

---
+++ app/code/local/Teamto/Featured/Block/Featured/List.php
@@ -0,0 +1,9 @@
+<?php
+
+//Mage_Adminhtml_Block_Catalog_Product_Grid
+class Teamto_Featured_Block_Featured_List extends Mage_Adminhtml_Block_Catalog_Product {
+
+    public function getname(){
+        return 'datttttt';
+    }
+}
--- app/code/local/Teamto/Featured/controllers/FeaturedController.php
+++ app/code/local/Teamto/Featured/controllers/FeaturedController.php
@@ -4,7 +4,6 @@ class Teamto_Featured_FeaturedController extends Mage_Adminhtml_Controller_Actio
 
     public function indexAction()
     {
-//        echo 'dat';die;
         $this->loadLayout();
         $this->renderLayout();
     }
